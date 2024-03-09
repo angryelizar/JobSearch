73cf6a5a1e19e2b6f093f28b91446e2eae005880
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-03-09 18:53:15 +0600

    Wrote a query to create the contact_types table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,18 @@
-create table if not exists users (
-    id long primary key auto_increment not null,
-    name text not null,
-    surname text not null,
-    email text unique not null,
+create table if not exists users
+(
+    id           long primary key auto_increment not null,
+    name         text                            not null,
+    surname      text                            not null,
+    email        text unique                     not null,
     phone_number varchar(55) unique,
-    password text not null,
-    avatar text,
-    age integer,
-    account_type varchar(50) not null,
+    password     text                            not null,
+    avatar       text,
+    age          integer,
+    account_type varchar(50)                     not null
+);
+
+create table if not exists contact_types
+(
+    id long primary key auto_increment not null,
+    type text not null
 );
