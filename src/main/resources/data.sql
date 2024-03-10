8f2ed6f75dd489b632728e299d49bd1808be86fe
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-03-10 16:05:11 +0600

    Created two vacancies from the employer

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -44,3 +44,9 @@
 -- values
 --     (2, 'Bold Brands Education', 'SMM за полгода',  '2015-03-01', '2015-06-01', 'SMM-менеджер'),
 --     (3, 'Attractor School', 'Java за полгода',  '2016-03-01', '2016-06-01', 'Junior Java-developer');
+
+-- Создаем две вакансии от работодателя
+-- insert into VACANCIES (NAME, DESCRIPTION, CATEGORY_ID, SALARY, EXP_FROM, EXP_TO, IS_ACTIVE, AUTHOR_ID, CREATED_DATE, UPDATE_TIME)
+-- values
+--     ('SMM-специалист', 'Нам нужен классный SMM-специалист', (select id from CATEGORIES where CATEGORIES.NAME = 'Маркетинг'), 150000, 2, 5, true, 2, '2024-03-01', '2024-03-08'),
+--     ('Java-разработчик', 'Нам нужен классный разработчик', (select id from CATEGORIES where CATEGORIES.NAME = 'IT'), 250000, 1, 2, true, 2, '2024-03-02', '2024-03-09');
