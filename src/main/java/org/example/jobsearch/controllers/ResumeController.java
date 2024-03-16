a957ed821a464ffbba6841441ea7be380046ff3a
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-03-16 18:28:54 +0600

    Added a new migration - removed not null in the user's last name column

--- src/main/java/org/example/jobsearch/controllers/ResumeController.java
+++ src/main/java/org/example/jobsearch/controllers/ResumeController.java
@@ -9,9 +9,7 @@ import org.example.jobsearch.exceptions.UserNotFoundException;
 import org.example.jobsearch.service.ResumeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,7 +34,6 @@ public class ResumeController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
-
     @GetMapping("resumes/user/{id}")
     public ResponseEntity<?> getResumesByUserId(@PathVariable int id){
         try {
