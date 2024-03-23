3a250f89f6f0965291ee4b8065b907eebfb68495
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-03-23 18:53:46 +0600

    Wrote a method and adapters to configure our database and Spring Security

--- src/main/java/org/example/jobsearch/config/SecurityConfig.java
+++ src/main/java/org/example/jobsearch/config/SecurityConfig.java
@@ -1,11 +1,33 @@
 package org.example.jobsearch.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        String authoritiesQuery = """
+                select u.EMAIL, a.AUTHORITY from ROLES ua, AUTHORITIES a, USERS u
+                where ua.AUTHORITY_ID = a.id
+                and ua.USER_ID = u.ID and u.EMAIL = ?
+                """;
+        String userQuery = """
+                select EMAIL, PASSWORD, ENABLED from USERS
+                WHERE email = ?;
+                """;
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(userQuery)
+                .authoritiesByUsernameQuery(authoritiesQuery);
+    }
 }
