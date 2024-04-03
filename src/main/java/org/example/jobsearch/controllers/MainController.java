d962d98c60c64304a9061c6610211374a6d07083
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-04-04 01:50:32 +0600

    Implemented display of the profile registration page

--- src/main/java/org/example/jobsearch/controllers/MainController.java
+++ src/main/java/org/example/jobsearch/controllers/MainController.java
@@ -1,17 +1,27 @@
 package org.example.jobsearch.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.example.jobsearch.dto.UserDto;
+import org.example.jobsearch.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
+    private final UserService userService;
 
     @GetMapping("registration")
     public String registrationGet(Model model){
         model.addAttribute("pageTitle", "Регистрация");
         return "main/registration";
+    }
+
+    @PostMapping("registration")
+    public String registrationPost(UserDto userDto){
+        userService.createUser(userDto);
+        return "redirect:/";
     }
 }
