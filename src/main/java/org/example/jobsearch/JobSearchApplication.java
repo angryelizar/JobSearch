4343e3cba249b3cbc970d59662ccd76a0b6052ea
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-04-14 10:53:50 +0600

    Added MVC Configurer setting for connecting JS files

--- src/main/java/org/example/jobsearch/JobSearchApplication.java
+++ src/main/java/org/example/jobsearch/JobSearchApplication.java
@@ -2,12 +2,19 @@ package org.example.jobsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class JobSearchApplication {
+public class JobSearchApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(JobSearchApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
 }
