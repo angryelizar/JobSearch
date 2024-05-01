5051387285368cd15e742e7ca65d060523b3c8fa
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-05-01 18:48:44 +0600

    Annotated the ContactType model

--- src/main/java/org/example/jobsearch/models/ContactType.java
+++ src/main/java/org/example/jobsearch/models/ContactType.java
@@ -1,5 +1,6 @@
 package org.example.jobsearch.models;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,7 +8,11 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "CONTACT_TYPES")
 public class ContactType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String type;
 }
