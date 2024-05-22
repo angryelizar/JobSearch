ee35c6157231336f3618a5fad01d5d9c17f2de6a
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-05-22 22:01:10 +0600

    Just fixed last migration

--- src/main/resources/db/changelog/initial/034_insert_values_of_locales_for_users.sql
+++ src/main/resources/db/changelog/initial/034_insert_values_of_locales_for_users.sql
@@ -2,11 +2,11 @@
 --changeset angryelizar:34-update-users-data
 
 update USERS
-SET preferred_locale = 'ru_KG'
+SET preferred_locale = 'ru'
 where EMAIL = 'conovalov.elizar@gmail.com';
 
 update USERS
-SET preferred_locale = 'ky_KG'
+SET preferred_locale = 'ky'
 where EMAIL = 'attractor@attractor.kg';
 
 update USERS
